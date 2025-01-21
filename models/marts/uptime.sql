691160d2c78c13720e71fbb7bfd2785cfd526fac
Chris Combs <combsc@umich.edu>
2025-01-21 15:21:50 -0500

    add uptime percentage, remove unnecessary union distinct calls

--- models/marts/uptime.sql
+++ models/marts/uptime.sql
@@ -1,8 +1,19 @@
+with merged_alarms_last_thirty_days as (
+    select
+        cce_id,
+        island_id,
+        MAX(interval_stop) - MIN(interval_begin) as actual_time_spent
+    from {{ ref('uptime_islands') }}
+    group by cce_id, island_id
+)
+
 select
     cce_id,
     (
-        extract(epoch from interval '15 days')
-        / extract(epoch from interval '30 days')
-    ) as uptime_percentage
-from {{ ref('all_alarms') }}
+        EXTRACT(epoch from interval '30 days')
+        - EXTRACT(epoch from SUM(actual_time_spent))
+    )
+    / EXTRACT(epoch from interval '30 days')
+    * 100 as uptime_percentage
+from merged_alarms_last_thirty_days
 group by cce_id
